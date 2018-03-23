19ec3030af127de75124fd408f0002ab3b7d62e3
laugiera <laugier.anne.a@gmail.com>
2018-03-23 14:22:42 +0100

    first component

---
+++ my-app/src/components/home/Home.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import "./Home.css"
+
+//Always start component names with a capital letter !!!!
+export default class Home extends Component {
+  constructor() {
+    super(); //pass props from the parent on the child
+    this.state = {}; //initial state = empty object
+  }
+
+  //In the method name : will means before it happens, did means after
+  componentWillMount(){
+    fetch('https://randomuser.me/api/') // temporary address
+    .then((results) => {//how we want the data to be returned
+      let json = results.json();
+      return json;
+    })
+    .then(json => {
+      this.setState({
+        title : json.results[0].name.title,
+        first : json.results[0].name.first,
+        last : json.results[0].name.last,
+      });
+      console.log(this.state);
+    })
+  }
+
+  render(){
+    return (
+      <div className="home">
+        {this.state.title}   {this.state.first}   {this.state.last}
+      </div>
+    )
+  }
+
+}

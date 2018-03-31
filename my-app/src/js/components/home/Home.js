d73ceeb6380447f36be179c660866251da9004c8
laugiera <laugier.anne.a@gmail.com>
2018-03-31 14:10:25 +0200

    fix back

--- my-app/src/js/components/home/Home.js
+++ my-app/src/js/components/home/Home.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component} from "react"
 import "./Home.css"
 
 //Always start component names with a capital letter !!!!
@@ -11,18 +11,18 @@ export default class Home extends Component {
   //In the method name : will means before it happens, did means after
   componentWillMount(){
     //fetch('https://randomuser.me/api/') // temporary address
-    fetch('/src/php/')
+    fetch("/src/php/public")
     .then((results) => {//how we want the data to be returned
-      let json = results.text();
-      console.log(json);
-      return json;
+      //let json = results.json()
+      console.log( results.json())
+      //return json
     })
-    .then(json => {
+  /*  .then(json => {
       this.setState({
         data : json
-      });
+      })
       console.log(this.state);
-    })
+    })*/
   }
 
   render(){
--- my-app/src/php/routes/web.php
+++ my-app/src/php/routes/web.php
@@ -17,6 +17,6 @@ $router->get('/hello/{name}', function ($name){
 $router->get('/', function(){
 	require("../app/Http/Controllers/requires.php");
 	$user = User::createFromId("beflgn735kjpamcrcn0plc95h4");
-	$str = "Bonjour Batman, votre voiture a un poids de ".$user->poids."kg et va à ".$user->vitesse." km/h";
-	echo json_encode($str);
+	$str = array("phrase" => "Bonjour Batman, votre voiture a un poids de ".$user->poids."kg et va à ".$user->vitesse." km/h");
+	return json_encode($str);
 });

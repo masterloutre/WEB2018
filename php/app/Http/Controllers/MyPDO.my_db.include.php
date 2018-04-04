5d19a7004d9e8c4f337ccc7156daa9ce70791890
masterloutre <williejunpow@gmail.com>
2018-04-04 22:10:00 +0200

    changement config db dans MyPDO.my_db.include.php pour test serveur

--- php/app/Http/Controllers/MyPDO.my_db.include.php
+++ php/app/Http/Controllers/MyPDO.my_db.include.php
@@ -3,4 +3,4 @@ require_once 'MyPDO.class.php';
 
 // TO DO : à modifier
 // host=votre serveur (localhost si travail en local)
-MyPDO::setConfiguration('mysql:host=localhost;dbname=bat;charset=utf8', 'root', '');
+MyPDO::setConfiguration('mysql:host=localhost;dbname=bat;charset=utf8', 'deniman', 'ProjetDéni');

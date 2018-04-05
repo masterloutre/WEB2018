e4b32d9b3c5503290ab69600a49dd07c931e4c86
LucasLavallee <lucas.lavallee@outlook.com>
2018-04-05 19:54:38 +0200

    class User + rooting

--- php/routes/web.php
+++ php/routes/web.php
@@ -11,12 +11,36 @@
 |
 */
 
-$router->get('/hello/{name}', function ($name){
+
+
+/*$router->get('user/{id}[/{type}]',function($id,$type = null){
+	$user = UserController::createFromId($id);
+	var_dump($user);
+	if(isset($type))
+		echo $user[0]->$type;
+	else
+		echo "Lol";
+});*/
+
+$router->get('/','UserController@createFromId');
+$router->get('user/{type}','UserController@createFromId');
+
+/*$router->get('/hello/{name}', function ($name){
     return "Hello $name !";
 });
+
+
 $router->get('/', function(){
 	require("../app/Http/Controllers/requires.php");
 	$user = User::createFromId("beflgn735kjpamcrcn0plc95h4");
-	$str = array("phrase" => "Bonjour Batman, votre voiture a un poids de ".$user->poids."kg et va à ".$user->vitesse." km/h");
+	$str = array("phrase" => "Bonjour Batman, votre voiture a un poids de ".$user->weight."kg et va à ".$user->speed." km/h");
 	return json_encode($str);
 });
+
+$router->get('/speed/{sessionId}','UserController@createFromId');*/
+
+/*$app->group(['namespace' => 'App\Http\Controllers'], function ($app) {
+    // Controllers Within The "App\Http\Controllers\Admin" Namespace
+    require("../app/Http/Controllers/requires.php");
+	$router->get('/speed/{sessionId}','UserController@speed');
+});*/

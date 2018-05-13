0d4b248fe219b7b6c225f4838470d30e9783a638
LucasLavallee <lucas.lavallee@outlook.com>
2018-05-13 11:38:04 +0200

    UserController + routing /car
    Remove sessions
    Add the new userID system
    Add method who's taking care of the mileage, oilLevel ... depending speed

--- php/routes/web.php
+++ php/routes/web.php
@@ -13,52 +13,71 @@
 
 
 /******* 
-	Rooting for /car/... 
+    Rooting for /car/... 
 *******/
 
 $router->group(['prefix' => 'car'], function () use ($router) {
-    $router->get('/','UserController@createFromId');
-    $router->get('{name}','UserController@get');
-    $router->put('{name}','UserController@set');
+    $router->get('/{id}','UserController@getAll');
+    $router->get('/','UserController@addNewEntry');
+    $router->put('/{id}','UserController@set');
 });
 
 /******* 
-	Rooting for /weapons/... 
+    Rooting for /weapons/... 
 *******/
 $router->group(['prefix' => 'weapons'], function () use ($router) {
-    $router->get('{id}','OwnController@getAmmunition');
+    $router->get('/','WeaponController@getAllWeapons');
+    $router->get('{weaponId}/user/{userId}','OwnController@getAmmunition');
     $router->put('{id}','OwnController@setAmmunition');
 });
 
 /******* 
+	Rooting for /car/... 
+*******/
+
+/*$router->group(['prefix' => 'car'], function () use ($router) {
+    $router->get('/','UserController@createFromId');
+    $router->get('{name}','UserController@get');
+    $router->put('{name}','UserController@set');
+});*/
+
+/******* 
+	Rooting for /weapons/... 
+*******/
+/*$router->group(['prefix' => 'weapons'], function () use ($router) {
+    $router->get('{id}','OwnController@getAmmunition');
+    $router->put('{id}','OwnController@setAmmunition');
+});*/
+
+/******* 
 	Rooting for /law/... 
 *******/
-$router->group(['prefix' => 'law'], function () use ($router) {
-    /*$router->get('scanner',);
-    $router->put('scanner',);*/
+/*$router->group(['prefix' => 'law'], function () use ($router) {
+    $router->get('scanner',);
+    $router->put('scanner',);
 
     $router->get('criminals/{id}','VilainController@createFromId');
     $router->patch('criminals/{id}','VilainController@patchVilain');
     $router->delete('criminals/{id}','VilainController@delete');
-    /*$router->post('criminals/{id}','VilainController@add');*/
-});
+    $router->post('criminals/{id}','VilainController@add');
+});*/
 
 /******* 
 	Rooting for /damage 
 *******/
-
-$router->get('damage','UserController@getCarbodyState');
+/*
+$router->get('damage','UserController@getCarbodyState');*/
 
 /******* 
 	Rooting for /driver 
-*******/
-$router->get('driver', 'UserController@getBpm');
+*******//*
+$router->get('driver', 'UserController@getBpm');*/
 
 /******* 
 	Rooting for /music/... 
-*******/
+*******//*
 $router->group(['prefix' => 'music'], function () use ($router) {
     $router->get('radio/{id}','RadioController@createFromId');
     $router->put('aux/{id}', function (){
     });
-});
+});*/

e4d9460bd8db40bb9a480880adc85741e2b13384
laugiera <laugier.anne.a@gmail.com>
2018-05-19 17:28:00 +0200

    gngngngngngn scanner police

--- js/src/components/batMobile/combatMode/centerPannelCombat/radioPolice/RadioPolice.js
+++ js/src/components/batMobile/combatMode/centerPannelCombat/radioPolice/RadioPolice.js
@@ -1,19 +1,47 @@
 import React, {Component} from "react"
 import "./RadioPolice.css"
 import axios from 'axios'
-
+import {DynLineGraph} from "../../../../dynLineGraph/DynLineGraph";
+import CurrentDate from "../../../../currentDate/CurrentDate";
 
 export default class RadioPolice extends Component {
-	
+
     constructor(props) {
         super(props);
-        this.state = {};
+				const currentDate = new Date()
+				this.state = {
+						time: currentDate.toTimeString(),
+						freq: 120
+				};
     }
+
+		componentDidMount() {
+				this.timerID = setInterval(
+						() => this.tick(),
+						1000
+				);
+		}
+
+		componentWillUnmount() {
+				clearInterval(this.timerID);
+		}
+
+		tick() {
+				const currentDate = new Date();
+				this.setState({
+						time: currentDate.toLocaleTimeString('fr-FR'),
+						freq: 120
+				})
+		}
 
     render() {
         return (
-            <div className="radio-police">
-            </div>
+					<div className="radioPolice row">
+							<div className="col-7">
+									<span> GCPD Scanner </span>
+									<DynLineGraph  data={211} unit={"hz/s"}/>
+							</div>
+					</div>
         );
     }
 }

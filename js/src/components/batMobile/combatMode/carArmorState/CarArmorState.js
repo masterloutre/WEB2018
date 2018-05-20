a1a7d1643ac019d6321399d91240830b8b371ab7
Thibault <rdthibault2@gmail.com>
2018-05-20 12:34:58 +0200

    carArmorState fixed

--- js/src/components/batMobile/combatMode/carArmorState/CarArmorState.js
+++ js/src/components/batMobile/combatMode/carArmorState/CarArmorState.js
@@ -3,77 +3,53 @@ import "./CarArmorState.css"
 import axios from 'axios'
 import CarArmorDiagram from "./carArmorDiagram/CarArmorDiagram";
 
-
 export default class CarArmorState extends Component {
 	
     constructor(props) {
         super(props);
-        () => this.fetchDamages();
         this.state = {
             carbodyState: 0,
             imagesDamagedSideView: "",
             imagesDamagedTopView: ""
         };
-       
     }
 
-            /*{
-                imageDamageUrl: ''
-                damaged0pct:this.loadImages(0),
-                damaged20pct:this.loadImages(20)
-            }*/
-
     componentDidMount(){
-        console.log("COMPONENT DID MOUNT!!!!")
-       // this.fetchDamages();
+        this.fetchDamages();
     }
 
     componentDidUpdate(prevProps, prevState){
         if(prevProps.sessionId !== this.props.sessionId) {
-            console.log("session id:", this.props.sessionId)
             this.fetchDamages();
-            //this.loadImages();
         }
     }
 
+    /*
+    * En fonction du pourcentage de carbodystate (taux d'endommagement de la batmobile) récupéré de la base de donnée, 
+    * on va calculer laquelle des 5 images d'états de la voiture il faut afficher, en arrondissant le pourcentage à la vingtaine la pkulus proche
+    * (car les images sont nommés d'après les vingtaines de pourcents de dommage)
+    */
     fetchDamages = () => {
         axios.get("/car/" + this.props.sessionId)
             .then((results) => {
                 const damageness = (Math.trunc(results.data.carbodyState / 20) * 20);
                 const imageDamagedSideUrl = require("../../../../images/batmobileDamaged/batmobileDamagedSide" + damageness + "prct.png");
                 const imageDamagedTopUrl = require("../../../../images/batmobileDamaged/batmobileDamagedTop" + damageness + "prct.png");
-                //console.log("image :", imageDamagedUrl);
                 this.setState({
                     carbodyState: results.data.carbodyState,
                     imagesDamagedSideView: imageDamagedSideUrl,
                     imagesDamagedTopView: imageDamagedTopUrl
                 })
-                console.log("YO", this.state)
-                /*this.setState({
-                    carbodyState: results.data.carbodyState
-                }, */
-                //this.loadImages(results.data.carbodyState)
             })
             .catch(error => console.log(error))
     }
 
-    /*loadImages = (carbodyState) => {
-        const imageDamagedUrl = require("../../../../images/batmobileDamaged/batmobileDamagedSide" + (Math.trunc(carbodyState / 20) * 20) + "pct.jpg");
-        console.log("image :", imageDamagedUrl);
-        this.setState({
-            carbodyState: carbodyState,
-            imagesDamagedSideView: imageDamagedUrl
-        }, console.log("hey le state !!!", this.state)) 
-    }*/
-
+    //On passe les url des images correspondant au taux de dégats issu de la BDD aux enfants
     render() {
-        console.log("debut du render", this.state.imagesDamagedSideView);
         return (
             <div className="car-armor-state container-fluid row p-2">
                 <CarArmorDiagram imagesUrl={this.state.imagesDamagedSideView} />
                 <CarArmorDiagram imagesUrl={this.state.imagesDamagedTopView} />
-
-            
             </div>
         );
     }
--- js/src/components/batMobile/combatMode/carArmorState/carArmorDiagram/CarArmorDiagram.js
+++ js/src/components/batMobile/combatMode/carArmorState/carArmorDiagram/CarArmorDiagram.js
@@ -7,12 +7,10 @@ export default class CarArmorDiagram extends Component {
 	
     constructor(props) {
         super(props);
-        
         this.state = {};
     }
 
     render() {
-        console.log("props car armor diagram render", this.props);
         return (
             <div className="col-xs-12 col-sm-6 car-armor-diagram">
                 <img src={this.props.imagesUrl} />
